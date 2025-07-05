5db1d743a5a70961970ee2abc1d57744f94ba91a
samsulhidayat123 <samaul1245sh@gmail.com>
2025-07-05 17:48:07 +0700

    Initial commit

---
+++ app/flask_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+import joblib, os
+from preprocessing import preprocess
+
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
+
+BASE_DIR = os.path.dirname(__file__)
+model = joblib.load(os.path.join(BASE_DIR, "models", "sentiment_model.pkl"))
+vectorizer = joblib.load(os.path.join(BASE_DIR, "models", "tfidf_vectorizer.pkl"))
+
+
+# 👉 Halaman prediksi
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    pred = None
+    if request.method == 'POST':
+        review = request.form['review']
+        vec = vectorizer.transform([preprocess(review)])
+        pred = model.predict(vec)[0]
+    return render_template('index.html', prediction=pred)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/preprocessing.py
@@ -0,0 +1,7 @@
+import re
+
+def preprocess(text):
+    text = str(text).lower()
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
